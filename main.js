27a3f70ef0eb021f60d4b72d7700bda836a62e20
Kalawela <kalawela.lo@gmail.com>
2021-10-14 21:23:50 -1000

    Now Working

--- main.js
+++ main.js
@@ -1,65 +1,96 @@
+ /*jshint esversion: 6 */
+
+const DOC = document;
+
+
 let MyGame =  {
     board: [],
     turn: 0,
     game: true,
     player_V_player: true,
-    init: function() {
-        this.restart_board();
-        console.log(this.board.length);
-        const parent_of_selected = document.getElementsByClassName("Board");
-        console.log(parent_of_selected.length)
-        for (let i = 0 ; i < this.board.length; i++){
-            let new_div_ = this.new_div(i); 
-            parent_of_selected[0].appendChild(new_div_);
-        }
-        console.log(this.board.length , " is may equal " , document.getElementsByClassName("Space").length)
+    wins: {
+        X: 0,
+        O: 0
     },
-    new_div: function(num){
-        var new_div = document.createElement("div");
+    ties: 0,
+    empty: "-_-",
+    init() {
+        this.restart_board();
+        var parent_of_selected = DOC.body.querySelector(".Board");
+        for(let i = 0 ; i < this.board.length; i++){          
+            let new_div_ = this.new_div(i);       
+            parent_of_selected.appendChild(new_div_);
+        }
+    }, new_div(num){
+        let new_div = DOC.createElement("div");
         new_div.classList.add("Space");
         new_div.id = num;
-        new_div.textContent = this.board[num];
-        new_div.onclick = this.checkinput(new_div.id)
-        return ;
-    },
-    draw: function() {
-        const sel = document.getElementById("Board");
-        const childs = sel.children;
+        new_div.textContent = this.empty;
+        new_div.addEventListener("click", function() {
+            MyGame.checkinput(new_div.id);
+        });
+        
+        return new_div;
+    }, draw() {
+        const sel = DOC.querySelector(".Board");
+        var childs = sel.childNodes;
         for (let i = 0 ; i < this.board.length; i++) {
-            let elem = childs.item(i)
+            let elem = childs.item(i);
             elem.textContent = this.board[i];
         }
-    },
-    checkinput: function(input_number) {
-        if(this.checkValidMove(input_number) && this.player_V_player == true){
+        const score_sel = DOC.querySelector("#Score");
+        score_sel.textContent = ("O: " + this.wins.O + " X: " + this.wins.X);
+    }, checkinput(input_number) {
+        if(this.checkValidMove(input_number)){
             this.turn += 1;
             let char_player = this.getPlayer();
             this.setMove(input_number, char_player);
+            this.checkWin();
             this.draw();
         }
-    },
-    setMove: function(num, char) {
+    }, setMove(num, char) {
         this.board[num] = char;
-    },
-    getPlayer: function() {
+    }, getPlayer() {
         let j = this.turn % 2;
-        if (j == 0){
+        if (j == 1){
             return "X";
         }
         return "O";
-    },
-    restart_board: function () {
-        this.board = [".",".","." ,".",".","." ,".",".","."]
-        this.turn = 0;
+    }, restart_board() {
+        this.board = [this.empty,this.empty,this.empty ,this.empty,this.empty,this.empty ,this.empty,this.empty,this.empty];
+        this.turn = 0;  
         return "Success";
-    },
-    checkValidMove: function(move_position) {
-        if (this.board[move_position] == "." ){
+    }, checkValidMove(move_position) {
+        if (this.board[move_position] == this.empty ){
             return true;
         }
         return false;
+    }, checkWin(){
+        const wins_states = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for(let i = 0; i < wins_states.length; i++){
+            if((this.board[wins_states[i][0]] == this.board[wins_states[i][1]]) &&
+                (this.board[wins_states[i][0]] == this.board[wins_states[i][2]]) &&
+                (this.board[wins_states[i][0]] != this.empty) ){
+                    let key = this.board[wins_states[i][0]];
+                    this.restart_board();
+                    return this.wins[key] += 1;
+            }
+        }
+        if(this.turn == 8){
+            this.restart_board();
+            this.ties += 1;
+        }
+         return;
     }
-
 };
 
 MyGame.init();
